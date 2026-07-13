e80be2a3304564285bbae76a8e5332aa2d3c1153
Raghavendra0416 <raghavendra.workin@gmail.com>
2026-07-13 15:06:09 +0530

    diff_response

--- Practice_1/basic_server.js
+++ Practice_1/basic_server.js
@@ -1,5 +1,16 @@
-import httpsModule from 'http'; // -> imported severModule
+import httpModule from 'http'; // -> imported severModule
 const PORT = 8089 //-> Provided a port number so anything connecting to server can listen
+// We can have any port 100, 200, 8286.
+// There are ports we cannot use like: 8081(used by OS), 8080 ...these are reserved Http, Https ports.
 
 
+//The below shows how to create a vaery basic server.
+const server = httpModule.createServer((req, res) => {
+    console.log("REQUEST RECIVED!");
+    res.end("Request Ended!, The Response is: Hello World!");
+});
 
+
+server.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`);
+})
---
+++ Practice_1/diff_response.js
@@ -0,0 +1,30 @@
+const httpModule = require('http');
+const PORT = 8089;
+const log = console.log;
+
+
+
+//How to check the different requests and handle the different responses.
+const server = httpModule.createServer((req, res) => {
+    const url = req.url;
+
+    if (url === "/") {
+        res.write("<h1>Welcome to Home Page</h1>")
+        res.write("Description about home page")
+        res.end();
+    } else if (url === "/about") {
+        res.write("<h1>About Page</h1>");
+        res.end();
+    } else if (url === "/contacts") {
+        res.write("<h1>Contacts</h1>");
+        res.end();
+    } else {
+        res.write("<h1>Page Does not exist</h1>");
+        res.end();
+    }
+});
+
+
+server.listen(PORT, () => {
+    log(`server is running at PORT: ${PORT}`);
+})
